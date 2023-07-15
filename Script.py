6ad9f6da3374771ec1ddc63e8c991efdca8953f0
xabhishekhar <72898173+xabhishekhar@users.noreply.github.com>
2023-07-15 14:47:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -28,9 +28,9 @@ class script(object):
 
 ⪼ 𝐄𝐱𝐚𝐦𝐩𝐥𝐞 ›
 
-<code>/batch https://t.me/mkn_bots_updates https://t.me/mkn_bots_updates</code>
+<code>/batch https://t.me/popcornpanda https://t.me/popcornpanda</code>
 
-𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/mkn_bots_updates><b>𝙼𝙺𝙽 𝙱𝙾𝚃𝚉</b></a>"""
+𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/popcornpanda><b>popcorn panda</b></a>"""
     
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
